95d0fbf8d94e53b5ed961c601dac5d20724912e0
cyberfrogg <68637510+cyberfrogg@users.noreply.github.com>
2022-02-15 02:15:58 +0300

    astronaut model

--- Assets/Plugins/Joystick Pack/Scripts/Joysticks/DynamicJoystick.cs
+++ Assets/Plugins/Joystick Pack/Scripts/Joysticks/DynamicJoystick.cs
@@ -13,19 +13,17 @@ public class DynamicJoystick : Joystick
     {
         MoveThreshold = moveThreshold;
         base.Start();
-        background.gameObject.SetActive(false);
+        background.gameObject.SetActive(true);
     }
 
     public override void OnPointerDown(PointerEventData eventData)
     {
         background.anchoredPosition = ScreenPointToAnchoredPosition(eventData.position);
-        background.gameObject.SetActive(true);
         base.OnPointerDown(eventData);
     }
 
     public override void OnPointerUp(PointerEventData eventData)
     {
-        background.gameObject.SetActive(false);
         base.OnPointerUp(eventData);
     }
 
